d58bf1cee543519296f13fd99337d81b98571df0
cocoslxdsgit <cocos1xd@FaKeYou.local>
2016-03-30 15:31:19 +0800

    fhadfhksdhf
    hdjfkahsdfjkhasldkf

---
+++ MyTestForCAShapeLayer/MyTestForCAShapeLayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTestForCAShapeLayer
+//
+//  Created by 酷酷的1xd on 16/2/15.
+//  Copyright © 2016年 酷酷的1xd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTestForCAShapeLayer/MyTestForCAShapeLayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyTestForCAShapeLayer
+//
+//  Created by 酷酷的1xd on 16/2/15.
+//  Copyright © 2016年 酷酷的1xd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
